74a3a22b64230e5d3926bc096080f26cd261092f
Claire Savard <csavard@claires-mbp.devices.brown.edu>
2019-01-08 14:29:46 -0500

    added description

--- gaussian_y.m
+++ gaussian_y.m
@@ -1,5 +1,17 @@
 function [y] = gaussian_y(b_p_data, params)
 
+% GAUSSIAN_Y is a weight function that weights the y parameter as a
+% gaussian with avg and stdev set by looking at lengths of repeated
+% sections on a histogram.
+%
+% input: B_P_DATA - nx2 matrix containing the birth and persistence
+%                   data
+%        PARAMS - contains the min and max value of the persistence
+%                 intervals  where you would like to linearly weight
+%                 the intervals between
+%
+% output: Y - weights associated to each interval
+
 min_x = params(1); %x is the interval length
 max_x = params(2);
 
